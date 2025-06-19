a2456c53e783c80279c2e1199b7fd03282211524
hamza0011 <44339149+hamza0011@users.noreply.github.com>
2025-06-19 17:05:12 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1042,8 +1042,8 @@ const handleFormSubmit = async (e) => {
                     <path d="M22 16.92v3a2 2 0 01-2.18 2 19.79 19.79 0 01-8.63-3.07 19.5 19.5 0 01-6-6 19.79 19.79 0 01-3.07-8.67A2 2 0 014.11 2h3a2 2 0 012 1.72 12.84 12.84 0 00.7 2.81 2 2 0 01-.45 2.11L8.09 9.91a16 16 0 006 6l1.27-1.27a2 2 0 012.11-.45 12.84 12.84 0 002.81.7A2 2 0 0122 16.92z"/>
                   </svg>
                 </div>
-                <div><a href="tel:+16076087411">+1 (607) 608-7411</a></div><br />
-                <div><a href="tel:+923174592503">+92 (317) 459-2503</a></div>
+                <a href="tel:+16076087411">+1 (607) 608-7411</a><br />
+                <a href="tel:+923174592503">+92 (317) 459-2503</a>
               </div>
               <div className="contact-info-item">
                 <div className="contact-icon">
@@ -1052,8 +1052,8 @@ const handleFormSubmit = async (e) => {
                     <circle cx="12" cy="10" r="3"/>
                   </svg>
                 </div>
-                <div><span>WorkVille 39 St. Midtown Manhattan, NY</span></div><br />
-                <div><span>Colabs Johar Town, Lahore, Pakistan</span></div>
+                <span>WorkVille 39 St. Midtown Manhattan, NY</span><br />
+                <span>Colabs Johar Town, Lahore, Pakistan</span>
               </div>
             </div>
           </div>
